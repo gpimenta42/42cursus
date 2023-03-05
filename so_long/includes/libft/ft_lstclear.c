0ccbab7b6437c5c4667d2025685847d8bb5d155d
Gaspar Pereira <87911989+gpimenta42@users.noreply.github.com>
2023-03-05 17:42:29 +0000

    Add files via upload

---
+++ so_long/includes/libft/ft_lstclear.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstclear.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpimenta <gpimenta@student.42lisboa.c      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 14:06:14 by gpimenta          #+#    #+#             */
+/*   Updated: 2022/10/24 14:06:18 by gpimenta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
+{
+	t_list	*current;
+	t_list	*next;
+
+	current = *lst;
+	while (current)
+	{
+		next = current->next;
+		(*del)(current->content);
+		free (current);
+		current = next;
+	}
+	*lst = NULL;
+}	
